91465bd6ab3c26f81067b2f7cb3295fc6caa88fd
nbaag <nbaagez@gmail.com>
2023-01-11 20:05:38 +0300

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
-import TodoItem from './components/TodoItem'
+import { useState } from "react";
+import TodoItem from "./components/TodoItem";
 
 const todos = [
-  { id: 123, title: "JavaScript", isComplite: false},
-  { id: 213, title: "Vue", isComplite: false},
-  { id: 321, title: "React", isComplite: false},
-]
+  { id: 123, title: "JavaScript", isComplite: false },
+  { id: 213, title: "Vue", isComplite: false },
+  { id: 321, title: "React", isComplite: false },
+];
 
 const App = () => {
   return (
-  <div className=''>
-    {todos.map(todo => (
-      <TodoItem key={todo.id} todo={todo} />
-    ))}
-  </div>
-  )
-}
+    <div className="bg-zinc-700 py-5 h-screen text-zinc-50">
+      <div className="w-11/12 mx-auto">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
+const TodoItem = ({ todo }) => {
+  return (
+    <div className="mx-auto lg:w-2/5 h-10 bg-zinc-600 rounded-md p-2 mb-3">
+      {todo.title}
+    </div>
+  );
+};
 
-const TodoItem = ({todo}) => {
-  return ( 
-    <div>{todo.title}</div>
-   );
-}
- 
 export default TodoItem;
